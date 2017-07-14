190649f47ea37501a309b1cc663bdd9ecdf35671
JatinderJet <goldy.jatinder@gmail.com>
2017-07-14 15:27:09 +0530

    updated the health check API's

--- inventory-service/solution/src/main/java/com/redhat/coolstore/rest/HealthCheckResource.java
+++ inventory-service/solution/src/main/java/com/redhat/coolstore/rest/HealthCheckResource.java
@@ -13,18 +13,11 @@ public class HealthCheckResource implements Serializable {
 
 	private static final long serialVersionUID = -7227732980791688773L;
 
-    @GET
-    @Path("/liveness")
-    @Health
-    public HealthStatus liveness() {
-        return HealthStatus.named("liveness").up();
-    }
-    
-    @GET
-    @Path("/readiness")
-    @Health
-    public HealthStatus readiness() {
-        return HealthStatus.named("readiness").up();
-    }
+	@GET
+	@Path("/ping")
+	@Health
+	public HealthStatus checkHealth() {
+		return HealthStatus.named("pong").up();
+	}
 
 }
